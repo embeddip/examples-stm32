c6e8c9b8a35a68c6a7aa2af5a2d2130d6d6d0db1
Ozan Durgut <ozandurgut.2001@hotmail.com>
2026-04-15 00:38:45 +0200

    initialize cpp apps branch

--- CoreCPP/Src/main.cpp
+++ CoreCPP/Src/main.cpp
@@ -1,48 +1,21 @@
-#include "main.h"
-#include "mnist_inference.h"
+#include "main.hpp"
 #include <embedDIP.hpp>
 
-using namespace embedDIP;
-
-#define IMG_SIZE 28
-#define NUM_CLASSES 10
+using namespace std;
 
 int application() {
+  embedDIP::Image inImg(IMAGE_RES_WQVGA, IMAGE_FORMAT_GRAYSCALE);
   embedDIP::Serial serial(&stm32_uart);
-  embedDIP::Image inImg(IMG_SIZE, IMG_SIZE, IMAGE_FORMAT_GRAYSCALE);
 
-  // Initialize serial
+  vector<int> histogram(256, 0);
+
   serial.init();
-
-  // Initialize MNIST model
-  if (mnist_init() != 0) {
-    // Initialization failed
-    while (1) {}
-  }
-
-  // Capture image from serial
   serial.capture(inImg);
 
-  // Get pixel data
-  uint8_t *pixels = (uint8_t *)inImg.pixels();
-
-  // Run inference
-  float output[NUM_CLASSES];
-  if (mnist_infer(pixels, output) != 0) {
-    // Inference failed
-    while (1) {}
-  }
-
-  // Get predicted digit
-  uint8_t predicted_digit = mnist_get_prediction(output);
-
-  // Send result back
-  serial.send1D(&predicted_digit, sizeof(uint8_t), 1, SERIAL_DATA_OTHER);
-
-  HAL_Delay(10);
+  inImg.histForm(histogram);
+  serial.send1D(histogram.data(), sizeof(int), 256, SERIAL_DATA_HISTOGRAM);
 
   while (1) {
     ;
   }
 }
-
